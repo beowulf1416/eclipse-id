5157ab50c1e2b8defe18c4d82343ce92b2972ef1
ferd tomale <beowulf1416@gmail.com>
2012-04-13 21:56:52 +0800

    added password dialog

---
+++ com.id.application/src/com/id/application/User.java
@@ -0,0 +1,20 @@
+package com.id.application;
+
+public class User {
+
+	public User(){
+		
+	}
+	
+	public boolean is_logged_in(){
+		return false;
+	}
+	
+	public void log_in(){
+		// TODO log_in
+	}
+	
+	public void log_out(){
+		// TODO log out
+	}
+}
---
+++ com.id.application/src/com/id/application/dialogs/PasswordDialog.java
@@ -0,0 +1,75 @@
+package com.id.application.dialogs;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class PasswordDialog extends TitleAreaDialog {
+	private Text txtName;
+	private Text txtPassword;
+
+	/**
+	 * Create the dialog.
+	 * @param parentShell
+	 */
+	public PasswordDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite area = (Composite) super.createDialogArea(parent);
+		Composite container = new Composite(area, SWT.NONE);
+		container.setLayout(new GridLayout(2, false));
+		container.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		Label lblUserName = new Label(container, SWT.NONE);
+		lblUserName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblUserName.setText("User name");
+		
+		txtName = new Text(container, SWT.BORDER);
+		txtName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblPassword = new Label(container, SWT.NONE);
+		lblPassword.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblPassword.setText("Password");
+		
+		txtPassword = new Text(container, SWT.BORDER | SWT.PASSWORD);
+		txtPassword.setText("");
+		txtPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		return area;
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
+				true);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 300);
+	}
+}
