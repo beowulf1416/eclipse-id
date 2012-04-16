d51287215e89a87b7aa687181c621711ef8de4c2
Ferd Tomale <beowulf1416@gmail.com>
2012-04-16 14:00:28 +0800

    modified initialize method on security data provider

--- com.id.security.provider.postgresql/src/com/id/security/provider/postgresql/PostgreSQLSecurityDataProvider.java
+++ com.id.security.provider.postgresql/src/com/id/security/provider/postgresql/PostgreSQLSecurityDataProvider.java
@@ -1,16 +1,50 @@
 package com.id.security.provider.postgresql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
+
+import org.eclipse.core.runtime.Status;
 
 import com.id.application.security.ISecurityDataProvider;
 
 public class PostgreSQLSecurityDataProvider implements ISecurityDataProvider {
 
+	private Connection _cn;
+	
+	/**
+	 * intializes the provider
+	 * @param Properties
+	 * 	should have the following properties:
+	 * 	* host
+	 *  * port
+	 *  * database
+	 *  * user
+	 *  * password
+	 * http://jdbc.postgresql.org/documentation/head/connect.html
+	 */
 	@Override
 	public void initialize(Properties properties) {
-		// TODO Auto-generated method stub
-
+		// jdbc:postgresql://host:port/database
+		String host = properties.getProperty("host", "");
+		String port = properties.getProperty("port", "");
+		String db = properties.getProperty("db", "");
+		
+		String uri = "jdbc:postgresql://"+host+":"+port+"/"+db;
+		
+		try {
+			_cn = DriverManager.getConnection(uri, properties);
+		} catch(SQLException e){
+			Activator.getDefault().getLog().log(
+				new Status(
+					Status.ERROR, 
+					Activator.PLUGIN_ID, 
+					"Unable to obtain a connection to a database instance"
+				)
+			);
+		}
 	}
 
 	@Override
